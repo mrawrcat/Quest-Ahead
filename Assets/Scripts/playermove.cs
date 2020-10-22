9103c87de2487031ea3175bb159c5ec15e666332
mrawrcat <wdan13@gmail.com>
2020-10-21 21:54:37 -0400

    made placeholder character

---
+++ Assets/Scripts/playermove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermove : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Vector2 movement, normMovement;
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        //movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        normMovement = movement.normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + normMovement * speed * Time.fixedDeltaTime);
+    }
+
+}
